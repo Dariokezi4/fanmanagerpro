9aecfb15b6ddbb076cc1e32a131ca0bd09bb8e84
Dariokezi4 <kezicdario30@gmail.com>
2025-05-22 23:41:14 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+
+export default function Home() {
+  return (
+    <div style={{ padding: "50px", textAlign: "center" }}>
+      <h1>👋 Dobrodošao u FanManager PRO</h1>
+      <p>Tvoja SaaS aplikacija za OnlyFans menadžere</p>
+    </div>
+  );
+}
